8e57878ad53546c45e556beea8854c5a019f1231
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-25 20:19:27 +0300

    drawing the turtle square

--- turtle_square.py
+++ turtle_square.py
@@ -10,8 +10,14 @@ timmy.right(90)
 timmy.forward(100)
 timmy.right(90)
 timmy.forward(100)
-timmy.right(45)
+timmy.right(90)
 
+"""
+More efficent way to do the above:
+for _ in range(4):
+    timmy.forward(100)
+    timmy.right(90)
+"""
 
 
 
