2480a332d5dd0fcbb7ba06501b052a5984bbc340
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-25 18:29:07 +0300

    drawing the turtle square

---
+++ turtle_square.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.shape("turtle")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(45)
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
