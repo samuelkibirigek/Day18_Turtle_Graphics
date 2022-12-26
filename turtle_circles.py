88879e484f3464e7da280acdaaed07b0543bb255
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-26 15:52:21 +0300

    spirograph of randomly colored circles of fixed radius

---
+++ turtle_circles.py
@@ -0,0 +1,25 @@
+import turtle as t
+import random
+
+timmy = t.Turtle()
+t.colormode(255)
+timmy.speed("fastest")
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    generated_color = (r, g, b)
+    return generated_color
+
+
+def spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.setheading(timmy.heading() + size_of_gap)
+
+spirograph(5)
+screen = t.Screen()
+screen.exitonclick()
